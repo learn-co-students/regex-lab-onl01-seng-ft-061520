777d19973e42269888ac39f8d93f9d4e8462cb51
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-29 14:39:41 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,14 +11,19 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  return_array = text.split("")
-  return_array.grep(/\b\w{5}\b/)
+  words = text.split(" ")
+  words.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    first_letter = text.scan(/\w+/)[0]
+    if (text.scan(/\./)) && (first_letter == first_letter.upcase)
+      true
+    else 
+      false
+    end
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/\b\(?\d{3}\)?\s?-?\d{3}-?\s?\d{4}\b/).empty? ? false : true
 end
