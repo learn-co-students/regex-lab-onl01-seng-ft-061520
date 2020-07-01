99424fcbc904f753fd6f90a02532ea0a19e640bb
Carlos Rosario <carlosrosario2k@gmail.com>
2020-07-01 13:43:50 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,3 +17,5 @@ def starts_with_a_vowel?(word)
   def valid_phone_number?(phone)
     phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
   end
+
+  # thats all folks
